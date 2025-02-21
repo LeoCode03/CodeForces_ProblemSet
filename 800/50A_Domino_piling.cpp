a74f70bbab961c1e49136794ca1e3709c4929fd9
Leandro Soliz <leandrosoliz310@gmail.com>
2025-02-21 11:02:29 -0400

    50A_Domino_piling

---
+++ 800/50A_Domino_piling.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int m, n, resul=0; cin>>m>>n;
+    resul = (n*m)/2;
+    cout<<resul;
+    return 0;
+}
---
+++ 800/50A_Domino_piling.py
@@ -0,0 +1,5 @@
+n, m = map(int, input().split())
+
+resul = (n*m)/2
+
+print(int(resul))
